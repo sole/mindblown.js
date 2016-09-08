e53805b1534ad4933167bab663e13eece9ffdfe8
sole <sole@mozilla.com>
2016-09-08 14:58:06 +0100

    using local variables

--- dist/mindblown.js
+++ dist/mindblown.js
@@ -43252,16 +43252,27 @@ function Slides(htmlSlides, options) {
 	EventEmitter.call(this);
 
 	var self = this;
+	var scene;
+	var camera;
+	var cameraTarget;
+	var renderer;
 
 	this.load = function() {
 		console.log('load');
 
 		// Hand over to the Loader to do the heavy lifting
+		// It will set some of our variables like sceneData
 		Loader(this, htmlSlides, options, (progress) => {
 			console.info('on progress', progress);
 			self.emit('load_progress', progress);
 		}, (complete) => {
 			console.info('completed load', complete);
+			
+			camera = self.sceneData.camera;
+			cameraTarget = self.sceneData.cameraTarget;
+			scene = self.sceneData.scene;
+			renderer = self.renderer;
+
 			self.emit('load_complete', complete);
 		});
 
@@ -43283,10 +43294,10 @@ function Slides(htmlSlides, options) {
 		currentRenderer.render(scene, camera);
 
 		 */
-		// TODO less this and more local variables
-		this.sceneData.camera.position.set(0, 0, 100);
-		this.sceneData.camera.lookAt(this.sceneData.cameraTarget);
-		this.renderer.render(this.sceneData.scene, this.sceneData.camera);
+		
+		camera.position.set(0, 0, 100);
+		camera.lookAt(this.sceneData.cameraTarget);
+		renderer.render(scene, camera);
 
 	};
 };
--- src/Slides.js
+++ src/Slides.js
@@ -11,16 +11,27 @@ function Slides(htmlSlides, options) {
 	EventEmitter.call(this);
 
 	var self = this;
+	var scene;
+	var camera;
+	var cameraTarget;
+	var renderer;
 
 	this.load = function() {
 		console.log('load');
 
 		// Hand over to the Loader to do the heavy lifting
+		// It will set some of our variables like sceneData
 		Loader(this, htmlSlides, options, (progress) => {
 			console.info('on progress', progress);
 			self.emit('load_progress', progress);
 		}, (complete) => {
 			console.info('completed load', complete);
+			
+			camera = self.sceneData.camera;
+			cameraTarget = self.sceneData.cameraTarget;
+			scene = self.sceneData.scene;
+			renderer = self.renderer;
+
 			self.emit('load_complete', complete);
 		});
 
@@ -42,10 +53,10 @@ function Slides(htmlSlides, options) {
 		currentRenderer.render(scene, camera);
 
 		 */
-		// TODO less this and more local variables
-		this.sceneData.camera.position.set(0, 0, 100);
-		this.sceneData.camera.lookAt(this.sceneData.cameraTarget);
-		this.renderer.render(this.sceneData.scene, this.sceneData.camera);
+		
+		camera.position.set(0, 0, 100);
+		camera.lookAt(this.sceneData.cameraTarget);
+		renderer.render(scene, camera);
 
 	};
 };
